a7a332e6fddb1f4ab8eee67c5b0325ea87de0039
ucukertz <45113402+ucukertz@users.noreply.github.com>
2021-04-27 09:41:29 +0700

    Non-local include

--- main/main.c
+++ main/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 //FreeRTOS Essentials
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
 //ESP system Essentials
-#include "esp_system.h"
-#include "esp_event.h"
-#include "esp_log.h"
-#include "nvs_flash.h"
+#include <esp_system.h>
+#include <esp_event.h>
+#include <esp_log.h>
+#include <nvs_flash.h>
 
 void app_main(void) {
 
